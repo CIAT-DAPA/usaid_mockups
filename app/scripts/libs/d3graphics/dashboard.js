c610bf7c177988e89e0a6fcb233e753feb2739ab
stevensotelo <stevenbetancurt@hotmail.com>
2016-08-24 16:14:48 -0500

    fixed the dashboard graphic

--- app/scripts/libs/d3graphics/dashboard.js
+++ app/scripts/libs/d3graphics/dashboard.js
@@ -88,12 +88,13 @@ D3Graphics.Dashboard.render = function (fData) {
             // call update functions of pie-chart and legend.    
             pC.update(nD);
             leg.update(nD);
+            $("#month").text(d[0]);
         }
 
         function mouseout(d) {    // utility function to be called on mouseout.
             // reset the pie-chart and legend.    
-            pC.update(tF);
-            leg.update(tF);
+            //pC.update(tF);
+            //leg.update(tF);
         }
 
         // create function to update the bars. This will be used by pie-chart.
@@ -146,27 +147,12 @@ D3Graphics.Dashboard.render = function (fData) {
         // Draw the pie slices.
         piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
             .each(function (d) { this._current = d; })
-            .style("fill", function (d) { return segColor(d.data.type); })
-            .on("mouseover", mouseover).on("mouseout", mouseout);
+            .style("fill", function (d) { return segColor(d.data.type); });
 
         // create function to update pie-chart. This will be used by histogram.
         pC.update = function (nD) {
             piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
                 .attrTween("d", arcTween);
-        }
-        // Utility function to be called on mouseover a pie slice.
-        function mouseover(d) {
-            // call the update function of histogram with new data.
-            /*hG.update(fData.map(function (v) {
-                return [v.State, v.freq[d.data.type]];
-            }), segColor(d.data.type));*/
-        }
-        //Utility function to be called on mouseout a pie slice.
-        function mouseout(d) {
-            // call the update function of histogram with all data.
-            /*hG.update(fData.map(function (v) {
-                return [v.State, v.total];
-            }), barColor);*/
         }
         // Animating the pie-slice requiring a custom function which specifies
         // how the intermediate paths should be drawn.
