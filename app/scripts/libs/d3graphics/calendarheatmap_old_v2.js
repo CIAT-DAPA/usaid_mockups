82490c3bb0e512ae9d69bf0f3e50e6131d38cd1b
stevensotelo <stevenbetancurt@hotmail.com>
2016-09-05 16:01:40 -0500

    change the library for the calendarheatmap

--- app/scripts/libs/d3graphics/calendarheatmap_old_v2.js
+++ app/scripts/libs/d3graphics/calendarheatmap_old_v2.js
@@ -11,50 +11,58 @@ D3Graphics.CalendarHeatmap.tools = {
 }
 
 D3Graphics.CalendarHeatmap.render = function (items) {
-    var width = 900,
-        height = 105,
-        cellSize = 12, // cell size
-        week_days = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sáb'],
-        month = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    var containerEl = document.getElementById(D3Graphics.CalendarHeatmap.vars.container.replace('#', ''));
 
-    var percent = d3.format(".1%"),
-        format = d3.time.format("%Y-%m-%d"),
-        round = d3.format(",.0f");/*,
-        parseDate = d3.time.format("%Y%m%d").parse;
+    var width = 960,
+        height = 136,
+        cellSize = 17; // cell size
 
-    /*var color = d3.scale.linear().range(["white", '#002b53'])
-        .domain([0, 1])*/
     var color = d3.scale.quantize()
         .domain([2000, 12000])
         .range(d3.range(11).map(function (d) { return "q" + d + "-11"; }));
 
+
+    var percent = d3.format(".1%"),
+        format = d3.time.format("%Y-%m-%d"),
+        round = d3.format(",.0f");
+
+
     var svg = d3.select(D3Graphics.CalendarHeatmap.vars.container).selectAll("svg")
         .data(d3.range(2016, 2017))
         .enter().append("svg")
-        .attr("width", '100%')
-        .attr("data-height", '0.5678')
-        .attr("viewBox", '0 0 900 105')
+        .attr("width", width)
+        .attr("height", height)
         .attr("class", "RdYlGn")
         .append("g")
         .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
 
     svg.append("text")
-        .attr("transform", "translate(-38," + cellSize * 3.5 + ")rotate(-90)")
+        .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
         .style("text-anchor", "middle")
         .text(function (d) { return d; });
 
-    for (var i = 0; i < 7; i++) {
-        svg.append("text")
-            .attr("transform", "translate(-5," + cellSize * (i + 1) + ")")
-            .style("text-anchor", "end")
-            .attr("dy", "-.25em")
-            .text(function (d) { return week_days[i]; });
-    }
+    /* Month titles */
+    var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    var titlesMonth = svg.selectAll('.year')
+        .selectAll('.titles-month')
+        .data(month)
+        .enter().append('g')
+        .attr('class', 'titles-month')
+        .attr('transform', function (d, i) {
+            return 'translate(' + (((i + 1) * (width / 12)) - 30) + ',-5)';
+        });
+
+    titlesMonth.append('text')
+        .attr('class', function (d, i) { return month[i]; })
+        .style('text-anchor', 'end')
+        .text(function (d, i) { return month[i]; });
+
+    console.log(titlesMonth);
 
     var rect = svg.selectAll(".day")
         .data(function (d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-        .enter()
-        .append("rect")
+        .enter().append("rect")
         .attr("class", "day")
         .attr("width", cellSize)
         .attr("height", cellSize)
@@ -62,25 +70,16 @@ D3Graphics.CalendarHeatmap.render = function (items) {
         .attr("y", function (d) { return d.getDay() * cellSize; })
         .datum(format);
 
-    var legend = svg.selectAll(".legend")
-        .data(month)
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function (d, i) { return "translate(" + (((i + 1) * 50) + 8) + ",0)"; });
-
-    legend.append("text")
-        .attr("class", function (d, i) { return month[i] })
-        .style("text-anchor", "end")
-        .attr("dy", "-.25em")
-        .text(function (d, i) { return month[i] });
+    rect.append("title")
+        .text(function (d) { return d; });
 
     svg.selectAll(".month")
         .data(function (d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
         .enter().append("path")
         .attr("class", "month")
-        .attr("id", function (d, i) { return month[i] })
         .attr("d", monthPath);
-       
+
+
     var data = d3.nest()
         .key(function (d) { return d.Fecha; })
         .rollup(function (d) { return d[0].RendimientoPromedio; })
@@ -88,8 +87,9 @@ D3Graphics.CalendarHeatmap.render = function (items) {
 
     rect.filter(function (d) { return d in data; })
         .attr("class", function (d) { return "day " + color(data[d]); })
-        .select("title")
-        .text(function (d) { return d + ": " + round(data[d]); });
+    /*.select("title")
+    .text(function (d) { return d + ": " + round(data[d]); });*/
+
 
     //  Tooltip Object
     var tooltip = d3.select("body")
@@ -130,4 +130,6 @@ D3Graphics.CalendarHeatmap.render = function (items) {
             + "H" + (w1 + 1) * cellSize + "V" + 0
             + "H" + (w0 + 1) * cellSize + "Z";
     }
+
+
 }
